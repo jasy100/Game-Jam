3a9131aba02dfe06cbe49dfcd4eef249a0c44d57
Pavlent1 <PavelKhmarukk@gmail.com>
2024-09-04 21:28:36 +0200

    2 controllers

---
+++ Game Jam/Assets/Scripts/PlayerInputSetter.cs
@@ -0,0 +1,24 @@
+using UnityEditor;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static UnityEngine.InputSystem.InputAction;
+using System;
+
+public class PlayerInputSetter : MonoBehaviour
+{
+    private Movement movement;
+    private PlayerInput PlayerInput;
+    void Awake()
+    {
+        PlayerInput = GetComponent<PlayerInput>();
+        var players = FindObjectsOfType<Movement>();
+        var index = PlayerInput.playerIndex;
+        movement = players.FirstOrDefault(m => m.GetPlayerIndex() == index);
+    }
+
+    public void OnMove(CallbackContext context)
+    {
+        movement.SetInputVector(context.ReadValue<Vector2>());
+    }
+}
