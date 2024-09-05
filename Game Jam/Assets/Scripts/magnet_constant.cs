2ac4eb7709b22089e5a8ce8eef84f51956a130a2
Bungaqueza <142844328+Bungaqueza@users.noreply.github.com>
2024-09-05 11:00:09 +0200

    added constant force

---
+++ Game Jam/Assets/Scripts/magnet_constant.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Constant : MonoBehaviour
+{
+    public GameObject objectToPull;
+    public float pullForce = 10f;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            // Calculate the direction vector from the object to pull to this object
+            Vector3 direction = transform.position - objectToPull.transform.position;
+
+            // Normalize the direction vector to get a unit vector
+            direction.Normalize();
+
+            // Apply the force to the object to pull
+            objectToPull.GetComponent<Rigidbody2D>().AddForce(direction * pullForce*Time.deltaTime);
+        }
+    }
+}
