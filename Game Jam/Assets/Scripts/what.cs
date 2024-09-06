06184703629c67c9585a42e4c5426b86be4bc700
Bungaqueza <142844328+Bungaqueza@users.noreply.github.com>
2024-09-06 11:58:51 +0200

    d

--- Game Jam/Assets/Scripts/what.cs
+++ Game Jam/Assets/Scripts/what.cs
@@ -7,7 +7,7 @@ public class what : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //sd
     }
 
     // Update is called once per frame
